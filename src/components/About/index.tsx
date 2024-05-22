03b0a198172e955849080ef7edc084d193f23b7c
Hector Robles <hjt.robles@gmail.com>
2024-05-21 21:19:06 -0400

    fix about alignment

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -7,7 +7,7 @@ export default function About() {
     <section className="py-24 bg-white bg-opacity-5">
       <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-screen-lg px-8">
         <div className="flex justify-center">
-          <div className="relative flex justify-end w-full max-w-96">
+          <div className="profile-container relative flex justify-end w-full">
             <img src="/crosses.png" className="absolute w-2/3 z-0 left-0" />
             <div className="relative profile-container flex justify-end items-end w-4/5">
               <div className="profile-bg absolute aspect-square w-full bottom-0 rounded-2xl right-0"></div>
@@ -21,7 +21,7 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="text-left">
+        <div className="text-left flex flex-col justify-center">
           <SectionHead>About Me</SectionHead>
           <p className="pb-4">
             As a developer with a designer's heart, I believe that websites and
