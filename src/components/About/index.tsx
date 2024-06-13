4a187ea1f93c1321177df1c1af803e7e1a34bb06
htrobles <hjt.robles@gmail.com>
2024-06-13 19:51:09 -0400

    include about animation

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
 import './About.scss';
 import SectionHead from '../SectionHead';
+import { Variants, motion } from 'framer-motion';
 
 export default function About() {
+  const container: Variants = {
+    show: {
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.75,
+      },
+    },
+  };
+
+  const image: Variants = {
+    hidden: {
+      opacity: 0,
+      scale: 0,
+    },
+    show: {
+      opacity: 1,
+      scale: 1,
+    },
+  };
+
+  const copyVariant: Variants = {
+    hidden: {
+      opacity: 0,
+      x: -32,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+    },
+  };
+
   return (
     <section id='about-me' className='py-24 bg-white bg-opacity-5'>
-      <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-screen-lg px-8'>
-        <div className='flex justify-center'>
-          <div className='max-w-[464px] relative flex justify-end w-full'>
-            <img
+      <motion.div
+        className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-screen-lg px-8'
+        variants={container}
+        initial='hidden'
+        whileInView='show'
+        // viewport={{ once: true }}
+      >
+        <motion.div className='flex justify-center' variants={container}>
+          <motion.div
+            className='max-w-[464px] relative flex justify-end w-full'
+            variants={container}
+          >
+            <motion.img
               src='/crosses.png'
               alt='Cross-graphics'
               className='absolute w-2/3 z-0 left-0'
+              variants={image}
             />
-            <div className='relative profile-container flex justify-end items-end w-4/5'>
+            <motion.div
+              className='relative profile-container flex justify-end items-end w-4/5'
+              variants={image}
+            >
               <div className='profile-bg absolute aspect-square w-full bottom-0 rounded-2xl right-0'></div>
               <div className='profile-bg-border absolute aspect-square w-full bottom-0 rounded-2xl right-0'></div>
               <img
@@ -22,20 +67,25 @@ export default function About() {
                 className='relative z-10 w-full object-contain rounded-b-2xl'
               />
               <div className='profile-border absolute aspect-square w-full bottom-0 rounded-2xl right-0 z-20'></div>
-            </div>
-          </div>
-        </div>
-        <div className='text-left flex flex-col justify-center'>
-          <SectionHead>About Me</SectionHead>
-          <p className='pb-4'>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          className='text-left flex flex-col justify-center'
+          variants={container}
+        >
+          <motion.div variants={copyVariant}>
+            <SectionHead>About Me</SectionHead>
+          </motion.div>
+          <motion.p className='pb-4' variants={copyVariant}>
             Hi, My name is{' '}
             <span className='font-bold text-primary-500'>Hector</span>. As a
             developer with a designer's heart, I believe that websites and web
             apps should be both functional and visually captivating. My approach
             is UI/UX-driven, prioritizing ease of use and comfort to ensure
             users achieve their goals effortlessly and enjoyably.
-          </p>
-          <p>
+          </motion.p>
+          <motion.p variants={copyVariant}>
             I code to breathe life into my designs. Unlike printed media,
             websites and apps offer a dynamic, interactive canvas for
             creativity. The endless possibilities of coding allow me to create
@@ -43,9 +93,9 @@ export default function About() {
             deployment, I thrive on every aspect of web development. My passion
             for both art and coding drives me to create seamless and beautiful
             user experiences.
-          </p>
-        </div>
-      </div>
+          </motion.p>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,30 +1,30 @@
 import './Hero.scss';
 import Button from '../Button';
-import { motion } from 'framer-motion';
+import { Variants, motion } from 'framer-motion';
 
 export default function Hero() {
-  const container = {
+  const container: Variants = {
     hidden: { opacity: 0 },
     show: {
       opacity: 1,
       transition: {
-        when: 'beforeChildren', //use this instead of delay
-        staggerChildren: 0.75, //apply stagger on the parent tag
+        when: 'beforeChildren',
+        staggerChildren: 0.5,
       },
     },
   };
 
-  const heading1 = {
+  const heading1: Variants = {
     hidden: { x: -100, opacity: 0 },
     show: { x: 0, opacity: 1 },
   };
 
-  const heading2 = {
+  const heading2: Variants = {
     hidden: { y: -32, opacity: 0 },
     show: { y: 0, opacity: 1 },
   };
 
-  const copyItem = {
+  const subhead: Variants = {
     hidden: { opacity: 0, y: 32 },
     show: {
       opacity: 1,
@@ -32,10 +32,14 @@ export default function Hero() {
     },
   };
 
-  const paragraph = {
-    hidden: { height: 0 },
+  const copyItem: Variants = {
+    hidden: {
+      opacity: 0,
+      x: -32,
+    },
     show: {
-      height: 'auto',
+      opacity: 1,
+      x: 0,
     },
   };
 
@@ -62,14 +66,13 @@ export default function Hero() {
         </motion.h1>
         <motion.h2
           className='text-white text-lg sm:text-xl md:text-2xl mb-8'
-          variants={copyItem}
+          variants={subhead}
         >
           Turn your ideas into great solutions.
         </motion.h2>
         <motion.p
           className='text-white mb-10 tracking-widest overflow-hidden'
-          variants={paragraph}
-          layout
+          variants={copyItem}
         >
           Transforming your concepts into high-performance applications is my
           specialty. From the initial brainstorming session to the final
@@ -78,11 +81,13 @@ export default function Hero() {
           robust web platforms, or creating innovative software solutions, your
           ideas are brought to life in extraordinary digital realities.
         </motion.p>
-        <motion.a href='#contact' variants={copyItem}>
-          <Button type='outline' color='white'>
-            Let's Collaborate
-          </Button>
-        </motion.a>
+        <motion.div variants={copyItem} className='w-fit'>
+          <a href='#contact'>
+            <Button type='outline' color='white'>
+              Let's Collaborate
+            </Button>
+          </a>
+        </motion.div>
       </motion.div>
     </div>
   );
