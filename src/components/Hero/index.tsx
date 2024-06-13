75eb0fb99e5d7eff04ee69265d7efde772bf6570
htrobles <hjt.robles@gmail.com>
2024-06-13 16:19:30 -0400

    add scroll to contact on collaborate button in hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,16 +4,16 @@ import Button from '../Button';
 
 export default function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <div className="container mx-auto px-8" style={{ maxWidth: 850 }}>
-        <h1 className="hero-copy font-display font-bold text-2xl sm:text-3xl md:text-4xl leading-none tracking-wide">
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <div className='container mx-auto px-8' style={{ maxWidth: 850 }}>
+        <h1 className='hero-copy font-display font-bold text-2xl sm:text-3xl md:text-4xl leading-none tracking-wide'>
           IF YOU CAN THINK IT,
           <br /> WE CAN BUILD IT.
         </h1>
-        <h2 className="text-white text-lg sm:text-xl md:text-2xl mb-8">
+        <h2 className='text-white text-lg sm:text-xl md:text-2xl mb-8'>
           Turn your ideas into great solutions.
         </h2>
-        <p className="text-white mb-10 tracking-widest">
+        <p className='text-white mb-10 tracking-widest'>
           Transforming your concepts into high-performance applications is my
           specialty. From the initial brainstorming session to the final
           deployment, every detail is carefully crafted to ensure top-notch
@@ -21,17 +21,19 @@ export default function Hero() {
           robust web platforms, or creating innovative software solutions, your
           ideas are brought to life in extraordinary digital realities.
         </p>
-        <Button type="outline" color="white">
-          Let's Collaborate
-        </Button>
+        <a href='#contact'>
+          <Button type='outline' color='white'>
+            Let's Collaborate
+          </Button>
+        </a>
       </div>
-      <div className="mx-auto absolute bottom-10 flex flex-col justify-center items-center">
+      {/* <div className='mx-auto absolute bottom-10 flex flex-col justify-center items-center'>
         <img
-          className="w-10 object-contain"
-          src="/double-down.svg"
-          alt="Scroll Down"
+          className='w-10 object-contain'
+          src='/double-down.svg'
+          alt='Scroll Down'
         />
-      </div>
+      </div> */}
     </div>
   );
 }
