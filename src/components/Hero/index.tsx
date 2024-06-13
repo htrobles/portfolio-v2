1a3a9746155eb1497f28571baa98517aebc97c84
htrobles <hjt.robles@gmail.com>
2024-06-13 19:11:03 -0400

    include hero animation

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,39 +1,89 @@
-import React from 'react';
 import './Hero.scss';
 import Button from '../Button';
+import { motion } from 'framer-motion';
 
 export default function Hero() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren', //use this instead of delay
+        staggerChildren: 0.75, //apply stagger on the parent tag
+      },
+    },
+  };
+
+  const heading1 = {
+    hidden: { x: -100, opacity: 0 },
+    show: { x: 0, opacity: 1 },
+  };
+
+  const heading2 = {
+    hidden: { y: -32, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+  };
+
+  const copyItem = {
+    hidden: { opacity: 0, y: 32 },
+    show: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  const paragraph = {
+    hidden: { height: 0 },
+    show: {
+      height: 'auto',
+    },
+  };
+
   return (
     <div className='h-screen flex flex-col justify-center items-center'>
-      <div className='container mx-auto px-8' style={{ maxWidth: 850 }}>
-        <h1 className='hero-copy font-display font-bold text-2xl sm:text-3xl md:text-4xl leading-none tracking-wide'>
+      <motion.div
+        className='container mx-auto px-8'
+        style={{ maxWidth: 850 }}
+        variants={container}
+        initial='hidden'
+        animate='show'
+      >
+        <motion.h1
+          className='hero-copy font-display font-bold text-2xl sm:text-3xl md:text-4xl leading-none tracking-wide'
+          variants={heading1}
+        >
           IF YOU CAN THINK IT,
-          <br /> WE CAN BUILD IT.
-        </h1>
-        <h2 className='text-white text-lg sm:text-xl md:text-2xl mb-8'>
+        </motion.h1>
+        <motion.h1
+          className='hero-copy font-display font-bold text-2xl sm:text-3xl md:text-4xl leading-none tracking-wide'
+          variants={heading2}
+        >
+          WE CAN BUILD IT.
+        </motion.h1>
+        <motion.h2
+          className='text-white text-lg sm:text-xl md:text-2xl mb-8'
+          variants={copyItem}
+        >
           Turn your ideas into great solutions.
-        </h2>
-        <p className='text-white mb-10 tracking-widest'>
+        </motion.h2>
+        <motion.p
+          className='text-white mb-10 tracking-widest overflow-hidden'
+          variants={paragraph}
+          layout
+        >
           Transforming your concepts into high-performance applications is my
           specialty. From the initial brainstorming session to the final
           deployment, every detail is carefully crafted to ensure top-notch
           results. Whether it's designing sleek mobile interfaces, developing
           robust web platforms, or creating innovative software solutions, your
           ideas are brought to life in extraordinary digital realities.
-        </p>
-        <a href='#contact'>
+        </motion.p>
+        <motion.a href='#contact' variants={copyItem}>
           <Button type='outline' color='white'>
             Let's Collaborate
           </Button>
-        </a>
-      </div>
-      {/* <div className='mx-auto absolute bottom-10 flex flex-col justify-center items-center'>
-        <img
-          className='w-10 object-contain'
-          src='/double-down.svg'
-          alt='Scroll Down'
-        />
-      </div> */}
+        </motion.a>
+      </motion.div>
     </div>
   );
 }
