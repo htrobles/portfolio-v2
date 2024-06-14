6dee850b2a63f83f644f30e24c532a8e5215f714
htrobles <hjt.robles@gmail.com>
2024-06-13 20:08:43 -0400

    animate timeline

--- src/components/Journey/index.tsx
+++ src/components/Journey/index.tsx
@@ -3,6 +3,7 @@ import SectionHead from '../SectionHead';
 import Timeline from './Timeline';
 import './Journey.scss';
 import JourneyCards from './JourneyCards';
+import { motion } from 'framer-motion';
 
 export default function Journey() {
   const [activeJourneyIndex, setActiveJourneyIndex] = useState(0);
@@ -16,22 +17,39 @@ export default function Journey() {
   };
 
   return (
-    <section id='my-journey' className='py-24 overflow-x-hidden'>
+    <motion.section
+      id='my-journey'
+      className='py-24 overflow-x-hidden'
+      onViewportLeave={() => setActiveJourneyIndex(0)}
+    >
       <div className='container mx-auto max-w-screen-lg px-8'>
-        <SectionHead>My Journey</SectionHead>
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+        >
+          <SectionHead>My Journey</SectionHead>
+        </motion.div>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-          <div className='order-1 lg:order-2 lg:mt-10'>
+          <motion.div
+            className='order-1 lg:order-2 lg:mt-10'
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+          >
             <Timeline activeJourneyIndex={activeJourneyIndex} />
-          </div>
-          <div className='journey-card-container order-2 lg:order-1 max-w-96'>
+          </motion.div>
+          <motion.div
+            className='journey-card-container order-2 lg:order-1 max-w-96'
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+          >
             <JourneyCards
               activeJourneyIndex={activeJourneyIndex}
               onClickPrevious={handleClickPrevious}
               onClickNext={handleClickNext}
             />
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
