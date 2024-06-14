d6f0fbf4f3b340c0b58223adbcfa496780993fdd
htrobles <hjt.robles@gmail.com>
2024-06-13 21:48:38 -0400

    animate tech

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SectionHead from '../SectionHead';
+import { Variants, motion } from 'framer-motion';
 
 type Technology = {
   src: string;
@@ -24,18 +25,83 @@ const technologies: Technology[] = [
 ];
 
 export default function Technologies() {
+  const container: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.125,
+      },
+    },
+  };
+
+  const tech: Variants = {
+    hidden: {
+      opacity: 0,
+      top: -32,
+    },
+    show: {
+      opacity: 1,
+      top: 0,
+    },
+  };
+
+  const head: Variants = {
+    hidden: {
+      opacity: 0,
+      left: -32,
+    },
+    show: {
+      opacity: 1,
+      left: 0,
+    },
+  };
+
+  const copy: Variants = {
+    hidden: {
+      opacity: 0,
+      bottom: -32,
+    },
+    show: {
+      opacity: 1,
+      bottom: 0,
+    },
+  };
+
   return (
     <section id='technologies' className='py-24'>
       <div className='container mx-auto max-w-screen-lg px-8'>
-        <SectionHead>Technologies</SectionHead>
+        <motion.div
+          variants={head}
+          initial='hidden'
+          whileInView='show'
+          className='relative'
+        >
+          <SectionHead>Technologies</SectionHead>
+        </motion.div>
         <div className='flex flex-col md:flex-row gap-8'>
-          <div className='grid grid-cols-3 sm:grid-cols-5 md:grid-cols-4 w-full gap-4 order-2 md:order-1'>
+          <motion.div
+            className='grid grid-cols-3 sm:grid-cols-5 md:grid-cols-4 w-full gap-4 order-2 md:order-1'
+            initial='hidden'
+            variants={container}
+            whileInView='show'
+          >
             {technologies.map(({ src, alt }) => (
-              <img key={alt} src={src} alt={alt} />
+              <motion.div variants={tech} className='relative'>
+                <img className='block' key={alt} src={src} alt={alt} />
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
           <div className='w-full order-1 md:order-2'>
-            <p>
+            <motion.p
+              variants={copy}
+              initial='hidden'
+              whileInView='show'
+              className='relative'
+            >
               At the heart of my craft lies a passion for innovation and
               exploration. While my expertise primarily centers around the MERN
               stack—utilizing MongoDB, Express.js, React, and Node.js to build
@@ -49,7 +115,7 @@ export default function Technologies() {
               diving into emerging trends or mastering established tools, I am
               committed to harnessing the power of technology to bring
               innovative solutions to life.
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
